be0b30283d478779943174d693106afd78d2cb35
Alex Westphal <alex@ahw.nz>
2017-12-29 15:57:58 +1300

    Added various parameter extractors to RequestContext

--- src/main/kotlin/nz/ahw/kaha/RequestContext.kt
+++ src/main/kotlin/nz/ahw/kaha/RequestContext.kt
@@ -1,3 +1,10 @@
+/*---------------------------------------------*\
+**                                             **
+**        Kaha Web Framework                   **
+**        Copyright 2018, Alex Westphal        **
+**        https://github.com/ahwnz/kaha        **
+**                                             **
+\*---------------------------------------------*/
 package nz.ahw.kaha
 
 import javax.servlet.http.HttpServletRequest
@@ -10,6 +17,19 @@ abstract class RequestContext {
 
     inner class Parameters {
 
-        operator fun get(name: String): String = request.getParameter(name)
+        operator inline fun <reified T> get(name: String): T? = when(T::class) {
+            Boolean::class -> getBoolean(name) as T
+            Double::class -> getDouble(name) as T
+            Int::class -> getInt(name) as T
+            Long::class -> getLong(name) as T
+            String::class -> getString(name) as T
+            else -> throw IllegalArgumentException("Can't extract '${T::class.java.name}' from a parameter")
+        }
+
+        fun getBoolean(name: String): Boolean? = request.getParameter(name)?.toBoolean()
+        fun getDouble(name: String): Double? = request.getParameter(name)?.toDoubleOrNull()
+        fun getInt(name: String): Int? = request.getParameter(name)?.toIntOrNull()
+        fun getLong(name: String): Long? = request.getParameter(name)?.toLongOrNull()
+        fun getString(name: String): String? = request.getParameter(name)
     }
 }
