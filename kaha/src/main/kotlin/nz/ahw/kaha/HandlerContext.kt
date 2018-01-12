b8db8c42c29f70ac95819f74968796cd4cdcf99b
Alex Westphal <alex@ahw.nz>
2018-01-13 08:44:44 +1300

    Added attributes and headers to HandlerContext

--- kaha/src/main/kotlin/nz/ahw/kaha/HandlerContext.kt
+++ kaha/src/main/kotlin/nz/ahw/kaha/HandlerContext.kt
@@ -7,7 +7,6 @@
 \*---------------------------------------------*/
 package nz.ahw.kaha
 
-import nz.ahw.kaha.http.StatusCode
 import nz.ahw.kaha.http.StatusCodes
 import javax.servlet.http.HttpServletRequest
 import kotlin.reflect.KClass
@@ -15,9 +14,32 @@ import kotlin.reflect.KClass
 @KahaDSL
 class HandlerContext(val request: HttpServletRequest) {
 
+    val attributes = Attributes(this)
+    val headers = Headers(this)
     val parameters = Parameters(this)
 
     fun signal(response: Response): Nothing = throw Signal(response)
+
+    class Attributes(val context: HandlerContext) {
+        operator inline fun <reified T: Any> get(name: String): T? = get(name, T::class)
+
+        @Suppress("UNCHECKED_CAST")
+        fun <T: Any> get(name: String, cls: KClass<T>): T? = context.request.getAttribute(name) as? T
+
+        fun getBoolean(name: String): Boolean? = context.request.getAttribute(name) as? Boolean
+        fun getDouble(name: String): Double? = context.request.getAttribute(name) as? Double
+        fun getInt(name: String): Int? = context.request.getAttribute(name) as? Int
+        fun getLong(name: String): Long? = context.request.getAttribute(name) as? Long
+        fun getString(name: String): String? = context.request.getAttribute(name) as? String
+
+        inline fun <reified T: Any> require(name: String, errorMessage: String): T = require(name, T::class, errorMessage)
+
+        fun <T: Any> require(name: String, cls: KClass<T>, errorMessage: String): T = get(name, cls) ?: context.signal(Responses.WithStatusCode(StatusCodes.BadRequest, errorMessage))
+    }
+
+    class Headers(val context: HandlerContext) {
+        operator fun get(name: String) = context.request.getHeader(name)
+    }
 
     class Parameters(val context: HandlerContext) {
 
