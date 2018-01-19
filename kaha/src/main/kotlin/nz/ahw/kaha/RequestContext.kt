000596b5398facb2cac2115022fe3dd42c6b85ef
Alex Westphal <alex@ahw.nz>
2018-01-20 12:30:41 +1300

    Added Session and ServletCOntrext attributes

--- kaha/src/main/kotlin/nz/ahw/kaha/RequestContext.kt
+++ kaha/src/main/kotlin/nz/ahw/kaha/RequestContext.kt
@@ -8,21 +8,33 @@
 package nz.ahw.kaha
 
 import nz.ahw.kaha.http.StatusCodes
+import javax.servlet.ServletContext
 import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpSession
 import kotlin.reflect.KClass
 
 interface RequestContext {
 
     val request: HttpServletRequest
+    val servletContext: ServletContext get() = request.servletContext
 
     val HttpServletRequest.attributes: RequestAttributes get() = RequestAttributes(this)
     val HttpServletRequest.headers: RequestHeaders get() = RequestHeaders(this)
     val HttpServletRequest.parameters: RequestParameters get() = RequestParameters(this)
 
+    val HttpSession.session: SessionAttributes get() = SessionAttributes(this)
+
+    val ServletContext.attributes: ServletContextAttributes get() = ServletContextAttributes(this)
+
     fun signal(response: Response): Nothing = throw Signal(response)
 }
 
-class RequestAttributes(val request: HttpServletRequest) {
+class RequestAttributes(private val request: HttpServletRequest): Iterable<Pair<String, Any>> {
+    override fun iterator(): Iterator<Pair<String, Any>> =
+        request.attributeNames.asSequence().map { name -> Pair(name, request.getAttribute(name)) }.iterator()
+
+    operator fun <T> set(name: String, value: T) = request.setAttribute(name, value)
+
     inline operator fun <reified T: Any> get(name: String): T? = get(name, T::class)
 
     @Suppress("UNCHECKED_CAST")
@@ -39,11 +51,13 @@ class RequestAttributes(val request: HttpServletRequest) {
     fun <T: Any> require(name: String, cls: KClass<T>, errorMessage: String): T = get(name, cls) ?: throw Signal(Responses.WithStatusCode(StatusCodes.BadRequest, errorMessage))
 }
 
-class RequestHeaders(val request: HttpServletRequest) {
+class RequestHeaders(private val request: HttpServletRequest) {
     operator fun get(name: String): String = request.getHeader(name)
 }
 
-class RequestParameters(val request: HttpServletRequest) {
+class RequestParameters(private val request: HttpServletRequest): Iterable<Pair<String, String>> {
+    override fun iterator(): Iterator<Pair<String, String>> =
+        request.parameterNames.asSequence().map { name -> Pair(name, request.getParameter(name)) }.iterator()
 
     inline operator fun <reified T: Any> get(name: String): T? = get(name, T::class)
 
@@ -62,6 +76,51 @@ class RequestParameters(val request: HttpServletRequest) {
     fun getInt(name: String): Int? = request.getParameter(name)?.toIntOrNull()
     fun getLong(name: String): Long? = request.getParameter(name)?.toLongOrNull()
     fun getString(name: String): String? = request.getParameter(name)
+
+    inline fun <reified T: Any> require(name: String, errorMessage: String): T = require(name, T::class, errorMessage)
+
+    fun <T: Any> require(name: String, cls: KClass<T>, errorMessage: String): T = get(name, cls) ?: throw Signal(Responses.WithStatusCode(StatusCodes.BadRequest, errorMessage))
+}
+
+class SessionAttributes(private val session: HttpSession): Iterable<Pair<String, Any>> {
+    override fun iterator(): Iterator<Pair<String, Any>> =
+        session.attributeNames.asSequence().map { name -> Pair(name, session.getAttribute(name)) }.iterator()
+
+    operator fun <T> set(name: String, value: T) = session.setAttribute(name, value)
+
+    inline operator fun <reified T: Any> get(name: String): T? = get(name, T::class)
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T: Any> get(name: String, cls: KClass<T>): T? = session.getAttribute(name) as? T
+
+    fun getBoolean(name: String): Boolean? = session.getAttribute(name) as? Boolean
+    fun getDouble(name: String): Double? = session.getAttribute(name) as? Double
+    fun getInt(name: String): Int? = session.getAttribute(name) as? Int
+    fun getLong(name: String): Long? = session.getAttribute(name) as? Long
+    fun getString(name: String): String? = session.getAttribute(name) as? String
+
+    inline fun <reified T: Any> require(name: String, errorMessage: String): T = require(name, T::class, errorMessage)
+
+    fun <T: Any> require(name: String, cls: KClass<T>, errorMessage: String): T = get(name, cls) ?: throw Signal(Responses.WithStatusCode(StatusCodes.BadRequest, errorMessage))
+}
+
+class ServletContextAttributes(private val servletContext: ServletContext): Iterable<Pair<String, Any>> {
+
+    override fun iterator(): Iterator<Pair<String, Any>> =
+        servletContext.attributeNames.asSequence().map { name -> Pair(name, servletContext.getAttribute(name)) }.iterator()
+
+    operator fun <T> set(name: String, value: T) = servletContext.setAttribute(name, value)
+
+    inline operator fun <reified T: Any> get(name: String): T? = get(name, T::class)
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T: Any> get(name: String, cls: KClass<T>): T? = servletContext.getAttribute(name) as? T
+
+    fun getBoolean(name: String): Boolean? = servletContext.getAttribute(name) as? Boolean
+    fun getDouble(name: String): Double? = servletContext.getAttribute(name) as? Double
+    fun getInt(name: String): Int? = servletContext.getAttribute(name) as? Int
+    fun getLong(name: String): Long? = servletContext.getAttribute(name) as? Long
+    fun getString(name: String): String? = servletContext.getAttribute(name) as? String
 
     inline fun <reified T: Any> require(name: String, errorMessage: String): T = require(name, T::class, errorMessage)
 
